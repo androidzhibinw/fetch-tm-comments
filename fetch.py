6a9642246ae57e7f22ec36a9deffdb569f6bc9a0
Zhibin Wang <zhibinw@qti.qualcomm.com>
2015-12-10 15:17:38 +0800

    update 3
    Change-Id: Ibaf9b8a3aaba408042886c8c1384f3f414a0856c

--- fetch.py
+++ fetch.py
@@ -15,9 +15,9 @@ page='&currentPage='
 tagId='&tagId=620'
 url='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=1&append=0&content=1&posi=-1&picture=0'
 urlD='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=3&append=0&content=1&posi=-1&picture=0'
-urlbad='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=1&append=0&content=0&tagId=620'
-urlbad2='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=1&append=0&content=1&tagId=620&posi=-1&picture=0'
-
+urlbad='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=1&append=0&content=1&tagId=620&posi=-1&picture=0'
+urlbadD='https://rate.tmall.com/list_detail_rate.htm?itemId=44320847678&spuId=327809007&sellerId=1732577545&order=3&append=0&content=1&tagId=620&posi=-1&picture=0'
+index=0
 
 def getPage(page_id):
     myweb = rq.get(urlD+page+str(page_id))
@@ -26,7 +26,13 @@ def getPage(page_id):
     content = re.findall('\"rateList\":(\[.*?\])\,\"tags\"',myweb.text.encode('utf-8'))[0]
     myjson = json.loads(content)
     for item in myjson:
+        if len(item['rateContent'].encode('utf-8')) >150:
+            continue
+        global index
+        index += 1
+        print index,' :'
         print item['rateDate'].encode('utf-8'),item['rateContent'].encode('utf-8')
+        print "</br>"
     #print json.dumps(myjson,indent=4, separators=(',', ': '),ensure_ascii=False).encode('utf-8')
     #mytable = pd.read_json(content)
     #a=mytable['rateDate']
@@ -35,5 +41,5 @@ def getPage(page_id):
     #mytable.to_csv('mytable.txt')
 
 
-for i in range(1,50):
+for i in range(1,51):
     getPage(i)
